f86e36ffebfa473bd9f40a2389d8771649bff337
Analice <anadias.alice@gmail.com>
2022-11-12 20:18:36 -0300

    style: correção de ortografia

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import app from "./src/app/index";
 const PORT = 5000;
 
 app.listen(PORT , () => {
-    console.log(`Server is running in port = ${PORT}`);
+    console.log(`Server is running on port = ${PORT}`);
 });
