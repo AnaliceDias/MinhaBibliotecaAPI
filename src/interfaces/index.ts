0c6afc1889547339c4ffe5a409e2caef4eed3f61
Analice <anadias.alice@gmail.com>
2022-11-12 19:03:41 -0300

    style: mudança no nome do arquivo

--- src/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-import { estante , statusDosLivros , statusDosEmprestimos , emprestimos } from "@prisma/client";
-
-export type livroInterface = Omit< estante , "id" >
-export type statusDosEmprestimosInterface = Omit< statusDosEmprestimos , "id" >
-export type statusDosLivrosInterface = Omit< statusDosLivros , "id" >
-export type emprestimosInterface = Omit< emprestimos , "id" >
